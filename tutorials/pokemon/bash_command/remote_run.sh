3d204067ed0622fd7a2b3e11b0f9a65db9a81725
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-08-29 19:18:12 +0200

    update

--- tutorials/pokemon/bash_command/remote_run.sh
+++ tutorials/pokemon/bash_command/remote_run.sh
@@ -3,6 +3,12 @@ export MLFLOW_S3_ENDPOINT_URL='https://minio.lab.sspcloud.fr'
 export MLFLOW_TRACKING_URI='https://user-pengfei-531016.kub.sspcloud.fr/'
 export MLFLOW_EXPERIMENT_NAME="pokemon"
 
+run_name="default"
+data_url = "https://minio.lab.sspcloud.fr/pengfei/sspcloud-demo/pokemon-cleaned.csv"
 
+# set the hyper parameters
+n_estimator = 50
+max_depth = 30
+min_samples_split = 2
 
-python ../train_model.py {experiment_name} {run_name} {data_url} {n_estimator} {max_depth} {min_samples_split}
+python ../train_model.py ${MLFLOW_EXPERIMENT_NAME} ${run_name} ${data_url} ${n_estimator} ${max_depth} ${min_samples_split}
