c5af4cd2e91b1d5e19e497bfd87d93bbc39b9bf5
pengfei <liu.pengfei@hotmail.fr>
2022-09-19 09:59:17 +0000

    update

--- tutorials/pokemon/bash_command/local_run.sh
+++ tutorials/pokemon/bash_command/local_run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 export MLFLOW_S3_ENDPOINT_URL='https://minio.lab.sspcloud.fr'
-export MLFLOW_TRACKING_URI='https://user-pengfei-866801.kub.sspcloud.fr/'
+export MLFLOW_TRACKING_URI='https://user-pengfei-837304.kub.sspcloud.fr/'
 export MLFLOW_EXPERIMENT_NAME="pokemon"
 
 run_name="default"
