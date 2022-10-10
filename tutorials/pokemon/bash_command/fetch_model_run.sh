6fd3518b302cd1efee649056e0a5c9691295dbd9
pengfei <liu.pengfei@hotmail.fr>
2022-10-10 10:14:43 +0000

    update bash for new image version

--- tutorials/pokemon/bash_command/fetch_model_run.sh
+++ tutorials/pokemon/bash_command/fetch_model_run.sh
@@ -1,7 +1,7 @@
 export MLFLOW_S3_ENDPOINT_URL=https://minio.lab.sspcloud.fr
-export MLFLOW_TRACKING_URI='https://user-pengfei-837304.kub.sspcloud.fr/'
+export MLFLOW_TRACKING_URI='https://user-pengfei-287428.user.lab.sspcloud.fr/'
 
 
-root_path="/home/jovyan/work/MLOPS"
+root_path="/home/onyxia/work/MLOPS"
 
 python ${root_path}/tutorials/pokemon/fetch_model.py
--- tutorials/pokemon/bash_command/local_run.sh
+++ tutorials/pokemon/bash_command/local_run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 export MLFLOW_S3_ENDPOINT_URL='https://minio.lab.sspcloud.fr'
-export MLFLOW_TRACKING_URI='https://user-pengfei-837304.kub.sspcloud.fr/'
+export MLFLOW_TRACKING_URI='https://user-pengfei-287428.user.lab.sspcloud.fr/'
 export MLFLOW_EXPERIMENT_NAME="pokemon"
 
 run_name="default"
@@ -13,6 +13,6 @@ n_estimator="50"
 max_depth="30"
 min_samples_split="2"
 
-root_path="/home/jovyan/work/MLOPS"
+root_path="/home/onyxia/work/MLOPS"
 
 python ${root_path}/tutorials/pokemon/train_model.py ${MLFLOW_EXPERIMENT_NAME} ${run_name} ${data_url} ${n_estimator} ${max_depth} ${min_samples_split}
